03ccc75dcebf962d3c237850b7767343cd112ca2
Henrique Rezende <henrique.64@hotmail.com>
2024-06-09 18:31:37 -0300

    feat: task controller

---
+++ backend/src/interfaces/controllers/TaskController.js
@@ -0,0 +1,79 @@
+const Task = require("../../entities/Task");
+const CreateTask = require("../../use_cases/task/CreateTask");
+const ListTasks = require("../../use_cases/task/ListTasks");
+const UpdateTask = require("../../use_cases/task/UpdateTask");
+const DeleteTask = require("../../use_cases/task/DeleteTask");
+
+class TaskController {
+  constructor(taskRepository) {
+    this.taskRepository = taskRepository;
+  }
+
+  listTasks(req, res) {
+    const { projectId } = req.body;
+    const listTasks = new ListTasks(this.taskRepository);
+    listTasks
+      .execute(projectId)
+      .then((tasks) => res.status(200).send(tasks))
+      .catch((err) => res.status(400).send(err.message));
+  }
+
+  async createTask(req, res) {
+    try {
+      const {
+        title,
+        description,
+        status,
+        completedBy,
+        completedAt,
+        projectId,
+      } = req.body;
+      const task = new Task(
+        projectId,
+        title,
+        description,
+        status,
+        completedBy,
+        completedAt
+      );
+
+      const createTask = new CreateTask(this.taskRepository);
+      const createdTask = await createTask.execute(task);
+      res.status(201).send(createdTask);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+
+  async updateTask(req, res) {
+    try {
+      const { id } = req.params;
+      const { title, description, status, completedBy, completedAt } = req.body;
+      const updateTask = new UpdateTask(this.taskRepository);
+      const updatedTask = await updateTask.execute(id, {
+        title,
+        description,
+        status,
+        completedBy,
+        completedAt,
+      });
+      res.status(200).send(updatedTask);
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+
+  deleteTask(req, res) {
+    try {
+      const { id } = req.params;
+      const deleteTask = new DeleteTask(this.taskRepository);
+      deleteTask
+        .execute(id)
+        .then(() => res.status(200).send({ message: "Tarefa deletada!" }));
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+}
+
+module.exports = TaskController;
