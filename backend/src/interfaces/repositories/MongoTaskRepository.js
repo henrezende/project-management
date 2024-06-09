0ec2f9b6c68ebc1f1f4d55c7cc0b2aa0dbbc14be
Henrique Rezende <henrique.64@hotmail.com>
2024-06-09 18:32:04 -0300

    feat: task repository

---
+++ backend/src/interfaces/repositories/MongoTaskRepository.js
@@ -0,0 +1,26 @@
+const TaskModel = require("../gateways/TaskModel");
+
+class MongoTaskRepository {
+  async add(task) {
+    const newTask = new TaskModel(task);
+    return await newTask.save();
+  }
+
+  async findById(id) {
+    return await TaskModel.findOne({ id });
+  }
+
+  async findByProjectId(projectId) {
+    return await TaskModel.find({ projectId });
+  }
+
+  async update(id, updatedTask) {
+    return await TaskModel.findByIdAndUpdate(id, updatedTask, { new: true });
+  }
+
+  async delete(id) {
+    return await TaskModel.findByIdAndDelete(id);
+  }
+}
+
+module.exports = MongoTaskRepository;
